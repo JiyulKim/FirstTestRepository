3164b7cba05cc7e5d6732896d8ef12dbf078f0f5
Jiyul <Jiyul@Jiyul-PC>
2016-08-10 01:57:48 +0900

    새로추가했당

--- GitTest/src/Test0810.java
+++ GitTest/src/Test0810.java
@@ -3,6 +3,7 @@ public class Test0810 {
 
 	public static void main(String[] args) {
 		// 좀 되면 좋겠당...ㅌㅌ
+		//된건지 만건지~~ 왜 노헤드가 뜰까 ㅌㅌ 나도 몰겠당~~~
 
 	}
 
---
+++ GitTest/src/Test0810_2.java
@@ -0,0 +1,9 @@
+
+public class Test0810_2 {
+
+	public static void main(String[] args) {
+		//된거야 만거야~_~
+
+	}
+
+}
